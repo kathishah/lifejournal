9dae4d2c4009073812c31bc92530614d03bb0f78
Chintan Shah <kathi.shah@gmail.com>
2014-11-09 02:21:24 -0800

    adding db and model

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
+ruby '2.0.0'
 source "https://rubygems.org"
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'pg'
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'thin'
--- web.rb
+++ web.rb
@@ -1,4 +1,38 @@
 require 'sinatra'
-get '/' do
-  "Hello Sinatra"
+require 'sinatra/json'
+require 'data_mapper'
+require 'pg'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+#'postgres://user:password@hostname/database'
+
+class User
+  include DataMapper::Resource
+  property :id, Serial
+  property :email_address, Text
 end
+
+class Entry
+  include DataMapper::Resource
+  property :id, Serial
+  property :signature, Text
+  property :body, Text
+  property :created_at, DateTime
+  property :submitted_at, DateTime
+  belongs_to :user
+end
+
+class User
+  has n, :entries
+end
+
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+  json :msg => "Hello Sinatra"
+end
+
+#post '/users' do
+#  u = User.new(params[:user])
+#  if u.save
+
